f1be07ff9c26733fd6410194048eaad1ba1e827b
Yulia <yliaj600@gmail.com>
2019-07-22 22:26:23 +0300

    усложненный второй урок

---
+++ hard_lesson2/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//первый пункт 
+let week = [
+    document.write("Понедельник".italics()) ,
+    document.write("Вторник") ,
+    document.write("Среда") ,
+    document.write("Четверг") ,
+    document.write("Пятница") ,
+    document.write("Суббота".bold()) ,
+    document.write("Воскресенье".bold())
+];
+
+
+// второй пункт
+let arr =  ["3456" , "98456" , "75643" , "65432" , "09456" ,"30734" , "65432"];
+
+console.log( arr[0]);
+console.log( arr[2]);
+console.log( arr[5]);
